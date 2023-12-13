b1701a5e2f514dd6bdd828e99f566e0b0c7eb39f
Haydn King <h.king@synthace.com>
2023-12-13 18:27:45 +0000

    Swap to lazyvim

---
+++ nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    cmd = "Neotree",
+    opts = {
+      event_handlers = {
+        {
+          event = "file_opened",
+          handler = function(file_path)
+            -- auto close
+            -- vimc.cmd("Neotree close")
+            -- OR
+            require("neo-tree.command").execute({ action = "close" })
+          end,
+        },
+      },
+    },
+  },
+}
